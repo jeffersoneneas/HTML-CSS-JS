9b57c229d0da6e1b72611cfd59fbe197f4600c56
jeffersoneneas <jeffersoneneassilva@gmail.com>
2023-11-29 15:15:17 -0300

    primeiro modulo

---
+++ JS/desafio1.js
@@ -0,0 +1,24 @@
+/*Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas :
+1 - Preço do combustível;
+2 - Gasto médio de combustível do carro por KM;
+3 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem.
+*/
+console.log('Programa de viagem!');
+// preço do combustível
+const precoCombustivel = 5.60;
+
+// km por litro
+const kmPorLitro = 27;
+
+// distância da viagem
+let DistanciaDaViagem = 450;
+
+//calculo do resultado //
+let resultado = (DistanciaDaViagem / kmPorLitro)*5;
+
+// mostrar no console 
+console.log('O valor gasto na viagem é: ' +resultado.toFixed());
---
+++ JS/variaveis.js
@@ -0,0 +1,10 @@
+let variavel = 10;
+console.log(variavel);
+
+// subscreve
+ variavel = 20;
+
+const pi = 3.14;
+
+console.log("Olá mundo!!");
+console.log(variavel, variavel, pi);
